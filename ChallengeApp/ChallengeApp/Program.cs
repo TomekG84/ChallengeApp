ac4be8e5c0b2f93ee66dde2895d2b3bd799b9ed3
TomekG84 <t_goszczynski@wp.pl>
2023-01-29 13:46:41 +0100

    Program z zadaniem domowym. Dzień czwarty.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Witaj Świecie!");
+﻿string name = "Ewa";
+bool female = false;
+int age = 18;
+
+
+if (female && age < 30)
+{
+    Console.WriteLine("Kobieta w wieku poniżej 30. lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Kobieta o imieniu Ewa w wieku 33. lat");
+}
+else if (!female && age <= 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
